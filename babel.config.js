b3ad3d88418ee4c380ca06da96c9c8d51da3f0fb
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-05-21 19:21:27 -0300

    babel: add metro babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = (api) => {
     api.cache(true);
     return {
-        presets: ["babel-preset-expo"],
+        presets: [
+            "babel-preset-expo",
+            "module:metro-react-native-babel-preset",
+        ],
         plugins: [
             "@babel/plugin-proposal-export-namespace-from",
             "react-native-reanimated/plugin",
