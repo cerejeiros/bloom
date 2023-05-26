0b43c28397867faeaede4f5993035e0a76253c0a
Kaio Enzo <kaioenzobr@gmail.com>
2023-05-26 20:16:15 -0300

    fix: context provider
    Context was not providing value to child components access data.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import AuthContextProvider, { AuthContext } from "../context/AuthContext";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export default function Routes() {
     const { user } = useContext(AuthContext);
-
     return (
-        <NavigationContainer>
-            {user ? <AppRoutes /> : <AuthRoutes />}
-        </NavigationContainer>
+        <AuthContextProvider>
+            <NavigationContainer>
+                {user ? <AppRoutes /> : <AuthRoutes />}
+            </NavigationContainer>
+        </AuthContextProvider>
     );
 }
