87d0ba8a4088ce6d0ab1a4c56389b381433f783e
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-05-26 01:12:26 -0300

    button: now accepts additional overhides styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,32 @@
+// MIT License
+//
+// Copyright (c) 2023 cerejeiros
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import React from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 import { ButtonProps } from "react-native-elements";
 import colors from "../pallete";
 
 const styles = StyleSheet.create({
-    button: {
+    container: {
         marginTop: 10,
         alignItems: "center",
         justifyContent: "center",
@@ -25,13 +47,36 @@ const styles = StyleSheet.create({
     },
 });
 
-function Button(props: ButtonProps) {
-    const { onPress, title = "Enviar" } = props;
+const enum Defaults {
+    Title = "Título",
+    Press_Message = "Foi apertado!",
+}
+
+function defaultOnPress(): void {
+    // eslint-disable-next-line no-console
+    console.log(Defaults.Press_Message);
+}
+
+export default function Button(props: ButtonProps) {
+    const {
+        onPress = defaultOnPress,
+        title = Defaults.Title,
+        style,
+        titleStyle,
+    } = props;
+
+    // Button with title as custom element.
+    if (typeof title !== "string")
+        return (
+            <Pressable style={[styles.container, style]} onPress={onPress}>
+                <title />
+            </Pressable>
+        );
+
+    // Button with title as a string.
     return (
-        <Pressable style={styles.button} onPress={onPress}>
-            <Text style={styles.text}>{title}</Text>
+        <Pressable style={[styles.container, style]} onPress={onPress}>
+            <Text style={[styles.text, titleStyle]}>{title}</Text>
         </Pressable>
     );
 }
-
-export default Button;
