14b446678bf04cfaf3a970a533b80543b69f8419
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-05-25 23:35:50 -0300

    screens: rename splash to launch

---
+++ src/screens/launch/Launch.tsx
@@ -0,0 +1,40 @@
+// This page will always load first before any other.
+// It is the "launching screen", or "splash screen" of the application.
+// Initial idea: https://github.com/cerejeiros/mobile/issues/5
+
+import Entypo from "@expo/vector-icons/Entypo";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback, useState } from "react";
+import { Text, View } from "react-native";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            // This tells the splash screen to hide immediately! If we call this after
+            // `setAppIsReady`, then we may see a blank screen while the app is
+            // loading its initial state and rendering its first pixels. So instead,
+            // we hide the splash screen once we know the root view has already
+            // performed layout.
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
+
+    if (!appIsReady) {
+        return null;
+    }
+
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+            onLayout={onLayoutRootView}
+        >
+            <Text>SplashScreen Demo! 👋</Text>
+            <Entypo name="rocket" size={30} />
+        </View>
+    );
+}
