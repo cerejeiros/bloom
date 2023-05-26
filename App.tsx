daa01bae92a15039880ec4e762946b9bcb5d68d4
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-05-25 21:38:22 -0300

    app: remove useless styles

--- App.tsx
+++ App.tsx
@@ -1,23 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import colors from "./src/pallete";
 import Routes from "./src/routes";
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.rose[50],
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-        position: "absolute",
-        top: 50,
-        fontSize: 30,
-        textTransform: "uppercase",
-        fontWeight: "900",
-    },
-});
 
 export default function App() {
     return <Routes />;
