ed406144b2265df5fb8800e206e3d83eb591e044
Kaio Enzo <kaioenzobr@gmail.com>
2023-05-26 20:51:42 -0300

    feat: navigation of home and signUp screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useContext } from "react";
-import AuthContextProvider, { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export default function Routes() {
     const { user } = useContext(AuthContext);
     return (
-        <AuthContextProvider>
-            <NavigationContainer>
-                {user ? <AppRoutes /> : <AuthRoutes />}
-            </NavigationContainer>
-        </AuthContextProvider>
+        <NavigationContainer>
+            {user ? <AppRoutes /> : <AuthRoutes />}
+        </NavigationContainer>
     );
 }
