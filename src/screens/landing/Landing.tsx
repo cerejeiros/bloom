1abb037cd7ae899acc9b79bb653de49d83523f5a
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-05-25 23:03:43 -0300

    screens: add landing

---
+++ src/screens/landing/Landing.tsx
@@ -0,0 +1,4 @@
+// This page is the first to load when opening our application the first time.
+// It is an opening concise section for what our application does.
+// It may have a small show-case of features our application offer.
+// At the end it will show options to either log-in or sign-up.
