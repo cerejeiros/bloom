7a6d030d8f1e2b413e6e5fafae37485a75929971
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-06-01 21:15:31 -0300

    login.logo: remove text component

--- src/screens/signup/logo.tsx
+++ src/screens/signup/logo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
