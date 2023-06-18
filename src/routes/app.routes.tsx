15e4c7268a009a3c190447e21beccb62d4347a96
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-06-18 16:50:17 -0300

    routes in app: change native bar colours

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,8 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import * as NavigationBar from "expo-navigation-bar";
 import React from "react";
 import NavBar from "../components/nav_bar";
+import colors from "../pallete";
 
 type StackRoutesParams = {
     home: undefined;
@@ -11,8 +13,14 @@ type StackRoutesParams = {
 export type StackNavigatorRoutesProps =
     NativeStackNavigationProp<StackRoutesParams>;
 
+NavigationBar.setBackgroundColorAsync(colors.black_800);
+NavigationBar.setButtonStyleAsync("light");
+
 // the tab routes is called in the drawer routes
 function TabRoutes() {
+    NavigationBar.setBackgroundColorAsync(colors.black_800);
+    NavigationBar.setButtonStyleAsync("light");
+
     return <NavBar />;
 }
 
