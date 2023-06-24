013705dae064f95b90037ae7f4ab960306db5478
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-06-24 15:20:35 -0300

    status: log user_data and user

--- src/screens/status/Status.tsx
+++ src/screens/status/Status.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../../context/AuthContext";
 import { StackNavigatorRoutesProps } from "../../routes/app.routes";
 
 const styles = StyleSheet.create({
@@ -12,6 +13,11 @@ const styles = StyleSheet.create({
 });
 
 export default function Status() {
+    const { user, userData } = React.useContext(AuthContext);
+
+    console.log("user:", user);
+    console.log("user data:", userData);
+
     const navigation = useNavigation<StackNavigatorRoutesProps>();
     return (
         <View style={styles.container}>
