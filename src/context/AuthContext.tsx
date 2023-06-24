95e1b4840717d938db08aa002f7c22f8e2050d4d
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-06-24 15:20:00 -0300

    context: add userdata global fetcher

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,13 +2,15 @@ import { User } from "@supabase/supabase-js";
 import React, { createContext, ReactNode } from "react";
 import { ToastAndroid } from "react-native";
 import supabase from "../helpers/supabaseClient";
+import { UserData } from "../types/shared";
 
 export type AuthContextDataProps = {
-    signIn: (email: string, password: string) => void;
+    signIn: (email: string, password: string) => Promise<void>;
     signOut: () => void;
-    signUp: (email: string, password: string) => Promise<any>;
-    signUpData: (usern: string, birth: string) => Promise<any>;
+    signUp: (email: string, password: string) => Promise<void>;
+    signUpData: (usern: string, birth: string) => Promise<void>;
     user: User | null;
+    userData: UserData | null;
 };
 
 type AuthContextProviderProps = {
@@ -23,6 +25,26 @@ export default function AuthContextProvider({
     children,
 }: AuthContextProviderProps) {
     const [user, setUser] = React.useState<User | null>(null);
+    const [userData, setUserData] = React.useState<UserData | null>(null);
+
+    const fetchData = React.useCallback(
+        async (id: string | undefined) => {
+            const { data } = await supabase
+                .from("profiles")
+                .select("*")
+                .eq("id", id)
+                .limit(1)
+                .returns<UserData[]>();
+
+            if (!data) {
+                throw Error("AuthContext: could not fetch data profile.");
+            }
+
+            const fetchedData = data[0];
+            setUserData(fetchedData);
+        },
+        [setUserData]
+    );
 
     const signIn = React.useCallback(
         async (email: string, password: string) => {
@@ -38,11 +60,14 @@ export default function AuthContextProvider({
                     error?.message ?? "Houve um erro no login!",
                     ToastAndroid.LONG
                 );
-            } else setUser(data.user);
+            } else {
+                setUser(data.user);
+                fetchData(data.user.id);
+            }
 
             return Promise.resolve();
         },
-        []
+        [fetchData]
     );
 
     const signUp = React.useCallback(
@@ -81,9 +106,10 @@ export default function AuthContextProvider({
                 console.log(error);
                 ToastAndroid.show(error?.message ?? "erro", ToastAndroid.LONG);
             }
+            fetchData(userSupabase);
             return Promise.resolve();
         },
-        []
+        [fetchData]
     );
 
     const signOut = async () => {
@@ -94,8 +120,15 @@ export default function AuthContextProvider({
     };
 
     const memoizedFunctions = React.useMemo(
-        () => ({ signUp, signIn, user, signOut, signUpData }),
-        [signUp, signIn, user, signUpData]
+        () => ({
+            signUp,
+            signIn,
+            user,
+            signOut,
+            signUpData,
+            userData,
+        }),
+        [signUp, signIn, user, signUpData, userData]
     );
 
     return (
