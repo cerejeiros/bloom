db8790fdd6e29901db9a38662f4b0907ad0e3e29
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-07-06 02:41:58 -0300

    types: update database

--- src/types/database.ts
+++ src/types/database.ts
@@ -12,33 +12,33 @@ export interface Database {
             habits: {
                 Row: {
                     completed: number;
-                    created_at: string | null;
-                    days: string[] | null;
-                    end: string | null;
+                    created_at: string;
+                    days: string[];
                     id: number;
-                    profile_id: string | null;
-                    start: string | null;
-                    tasks: number | null;
+                    name: string[];
+                    period: string[];
+                    profile_id: string;
+                    routine_id: number | null;
                 };
                 Insert: {
                     completed?: number;
-                    created_at?: string | null;
-                    days?: string[] | null;
-                    end?: string | null;
+                    created_at?: string;
+                    days: string[];
                     id?: number;
-                    profile_id?: string | null;
-                    start?: string | null;
-                    tasks?: number | null;
+                    name?: string[];
+                    period?: string[];
+                    profile_id: string;
+                    routine_id?: number | null;
                 };
                 Update: {
                     completed?: number;
-                    created_at?: string | null;
-                    days?: string[] | null;
-                    end?: string | null;
+                    created_at?: string;
+                    days?: string[];
                     id?: number;
-                    profile_id?: string | null;
-                    start?: string | null;
-                    tasks?: number | null;
+                    name?: string[];
+                    period?: string[];
+                    profile_id?: string;
+                    routine_id?: number | null;
                 };
                 Relationships: [
                     {
@@ -48,9 +48,9 @@ export interface Database {
                         referencedColumns: ["id"];
                     },
                     {
-                        foreignKeyName: "habits_tasks_fkey";
-                        columns: ["tasks"];
-                        referencedRelation: "tasks";
+                        foreignKeyName: "habits_routine_id_fkey";
+                        columns: ["routine_id"];
+                        referencedRelation: "routines";
                         referencedColumns: ["id"];
                     }
                 ];
@@ -98,26 +98,41 @@ export interface Database {
             profiles_tasks: {
                 Row: {
                     completed: number;
-                    created_at: string | null;
+                    created_at: string;
+                    done: number;
+                    habit_id: number | null;
                     id: number;
-                    profile_id: string | null;
-                    task_id: number | null;
+                    priority: Database["public"]["Enums"]["task_priority"];
+                    profile_id: string;
+                    task_id: number;
                 };
                 Insert: {
                     completed?: number;
-                    created_at?: string | null;
+                    created_at?: string;
+                    done?: number;
+                    habit_id?: number | null;
                     id?: number;
-                    profile_id?: string | null;
-                    task_id?: number | null;
+                    priority?: Database["public"]["Enums"]["task_priority"];
+                    profile_id: string;
+                    task_id: number;
                 };
                 Update: {
                     completed?: number;
-                    created_at?: string | null;
+                    created_at?: string;
+                    done?: number;
+                    habit_id?: number | null;
                     id?: number;
-                    profile_id?: string | null;
-                    task_id?: number | null;
+                    priority?: Database["public"]["Enums"]["task_priority"];
+                    profile_id?: string;
+                    task_id?: number;
                 };
                 Relationships: [
+                    {
+                        foreignKeyName: "profiles_tasks_habit_id_fkey";
+                        columns: ["habit_id"];
+                        referencedRelation: "habits";
+                        referencedColumns: ["id"];
+                    },
                     {
                         foreignKeyName: "profiles_tasks_profile_id_fkey";
                         columns: ["profile_id"];
@@ -136,31 +151,25 @@ export interface Database {
                 Row: {
                     completed: number;
                     created_at: string | null;
-                    habits: number | null;
                     id: number;
+                    name: string[];
                     profile_id: string | null;
                 };
                 Insert: {
                     completed?: number;
                     created_at?: string | null;
-                    habits?: number | null;
                     id?: number;
+                    name?: string[];
                     profile_id?: string | null;
                 };
                 Update: {
                     completed?: number;
                     created_at?: string | null;
-                    habits?: number | null;
                     id?: number;
+                    name?: string[];
                     profile_id?: string | null;
                 };
                 Relationships: [
-                    {
-                        foreignKeyName: "routines_habits_fkey";
-                        columns: ["habits"];
-                        referencedRelation: "habits";
-                        referencedColumns: ["id"];
-                    },
                     {
                         foreignKeyName: "routines_profile_id_fkey";
                         columns: ["profile_id"];
@@ -173,36 +182,30 @@ export interface Database {
                 Row: {
                     created_at: string;
                     created_by: string;
-                    done: number | null;
                     id: number;
-                    name: string;
-                    period: string | null;
-                    priority: string | null;
-                    repeated: string | null;
+                    name: string[];
+                    period: string[];
+                    repeated: string;
                     shared: boolean;
                     times: number;
                 };
                 Insert: {
                     created_at?: string;
                     created_by: string;
-                    done?: number | null;
                     id?: number;
-                    name: string;
-                    period?: string | null;
-                    priority?: string | null;
-                    repeated?: string | null;
+                    name?: string[];
+                    period?: string[];
+                    repeated?: string;
                     shared?: boolean;
                     times?: number;
                 };
                 Update: {
                     created_at?: string;
                     created_by?: string;
-                    done?: number | null;
                     id?: number;
-                    name?: string;
-                    period?: string | null;
-                    priority?: string | null;
-                    repeated?: string | null;
+                    name?: string[];
+                    period?: string[];
+                    repeated?: string;
                     shared?: boolean;
                     times?: number;
                 };
@@ -216,7 +219,7 @@ export interface Database {
             [_ in never]: never;
         };
         Enums: {
-            task_priority: "urgent" | "high" | "medium" | "low";
+            task_priority: "low" | "medium" | "high";
             task_status: "done" | "in progress" | "disabled";
         };
         CompositeTypes: {
