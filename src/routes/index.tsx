c7db530c300faf636a36cbbcb3b7bbeca38a71f3
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-07-10 21:17:17 -0300

    routes: reload local data when possible, supabase otherwise

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,46 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import Loading from "../components/Loading";
-import { GlobalContext } from "../context/GlobalContext";
+import { useGlobalContext } from "../context/GlobalContext";
 import supabase from "../helpers/supabaseClient";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export default function Routes() {
-    const { user, userData, setUser, fetchData } =
-        React.useContext(GlobalContext);
+    const { user, userData, setUser, setUserData } = useGlobalContext();
     const [loading, setLoading] = React.useState(true);
 
     // Recupera a sessão do usuário quando o app é recarregado ou aberto novamente.
     React.useEffect(() => {
-        const refreshSession = async () => {
+        (async () => {
             setLoading(true);
-            const { data } = await supabase.auth.getSession();
-            if (data && data.session) {
-                setUser(data.session.user);
-                await fetchData(data.session.user.id);
+            try {
+                const value = await AsyncStorage.getItem("user");
+                if (value !== null) {
+                    await setUser(JSON.parse(value));
+                    const valueData = await AsyncStorage.getItem("user-data");
+                    if (valueData !== null)
+                        await setUserData(JSON.parse(valueData));
+                } else {
+                    const { data } = await supabase.auth.getSession();
+                    if (data && data.session) {
+                        await setUser(data.session.user);
+                        const valueData = await AsyncStorage.getItem(
+                            "user-data"
+                        );
+                        if (valueData !== null)
+                            await setUserData(JSON.parse(valueData));
+                    }
+                }
+            } catch (e) {
+                throw Error(
+                    `GlobalContext: getUser() -> Could not read user information. ${e}`
+                );
             }
             setLoading(false);
-        };
-        refreshSession();
-    }, [setUser, fetchData]);
-
+        })();
+    }, [setUser, setUserData]);
     return (
         <NavigationContainer>
             {user && userData ? (
